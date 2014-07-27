e6879d093cc430f772e59c69c33c2e39488f5fe5
karee1 <karikujanpaa89@hotmail.com>
2014-07-27 14:09:54 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The purpose of these functions is to cache time-consuming calculations
+## and therefore make the process faster. The makeCacheMatrix first caches
+## the mean of a matrix and cacheSolve returns the value of the calculation.
 
-## Write a short comment describing this function
+
+
+
+## makeCacheMatrix is attempting to inverse the matrix, calculate its mean
+## and cache it
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    m <- NULL
+    s <- NULL
+    set <- function(y){
+        x <<- y
+        m <<- NULL
+        s <<- NULL
+        
+    }
+    get <- function() x
+    setmean <- function(mean) m <<- mean
+    setinv <- function(solve) s <<- solve
+    getmean <- function() m
+    getinv <- function() s(m)
+    
+    list(set = set, get = get,
+         setinv = setinv,
+         setmean = setmean,
+         getmean = getmean, 
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns the mean of the matrix from cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getmean()
+    if(!is.null(m)){
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- mean(data,...)
+    x$setmean(m)
+    solve(m)
+    m
+    
 }
+
+
